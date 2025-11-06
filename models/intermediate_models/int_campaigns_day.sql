52b9ef61f8c6e9dbb6135e1679842fed15282f61
Geoffrey Bennett <geoffreymichael96@gmail.com>
2025-11-06 11:44:22 +0000

    updated campaign day model - removing paid_source

--- models/intermediate_models/int_campaigns_day.sql
+++ models/intermediate_models/int_campaigns_day.sql
@@ -1,13 +1,10 @@
 select 
     date_date
-    ,paid_source
-    --,campaign_key
-    --,campaign_name
     ,sum(ads_cost) as ads_cost
     ,sum(impression) as impressions
     ,sum(click) as clicks
 from {{ ref('int_campaigns') }}
 
-GROUP BY date_date, paid_source
+GROUP BY date_date
 
 order by date_date desc
