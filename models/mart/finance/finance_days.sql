d751f26e6fe3097d52ca3e4095936a815ee4b272
Geoffrey Bennett <geoffreymichael96@gmail.com>
2025-11-06 12:03:07 +0000

    created finance_campaign_day and a few other query tweaks from missed columns

--- models/mart/finance/finance_days.sql
+++ models/mart/finance/finance_days.sql
@@ -3,9 +3,11 @@ select
     date_date,
     count(orders_id) as nb_transactions,
     round(sum(revenue),2) as revenue,
+    round(sum(margin),2) as margin,
     round(sum(quantity) / count(orders_id),2) as average_basket,
     round(sum(operational_margin),2) as operational_margin,
     round(sum(purchase_cost),2) as purchase_cost,
+    round(sum(shipping_cost),2) as shipping_cost,
     round(sum(shipping_fee),2) as shipping_fees,
     round(sum(logcost),2) as total_log_costs,
     sum(quantity) as total_products_sold
